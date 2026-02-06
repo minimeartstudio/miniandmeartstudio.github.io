9333a02bcfa070ff24c5bdc35cc335667851d618
minimeartstudio <Laura.piantedosi@gmail.com>
2026-02-06 14:51:44 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,10 @@
-// Header shrink on scroll
+// Nav scroll behavior
 const header = document.querySelector(".topnav");
 
-function onScroll() {
-  if (window.scrollY > 18) {
-    header.classList.add("scrolled");
-  } else {
-    header.classList.remove("scrolled");
-  }
-}
+const onScroll = () => {
+  if (window.scrollY > 18) header.classList.add("scrolled");
+  else header.classList.remove("scrolled");
+};
 
 window.addEventListener("scroll", onScroll, { passive: true });
 onScroll();
