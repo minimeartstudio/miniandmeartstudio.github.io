ad2b45686009768ace87b9589642921290365d43
minimeartstudio <Laura.piantedosi@gmail.com>
2026-02-06 14:55:58 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 // Nav scroll behavior
 const header = document.querySelector(".topnav");
 
-const onScroll = () => {
+function onScroll() {
   if (window.scrollY > 18) header.classList.add("scrolled");
   else header.classList.remove("scrolled");
-};
+}
 
 window.addEventListener("scroll", onScroll, { passive: true });
 onScroll();
