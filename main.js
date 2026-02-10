855519ae2f08000eef4de0a9a3be2e35ca666d50
minimeartstudio <Laura.piantedosi@gmail.com>
2026-02-10 12:58:43 -0500

    Update main.js

--- main.js
+++ main.js
@@ -11,3 +11,19 @@
   window.addEventListener("scroll", onScroll, { passive: true });
   onScroll();
 })();
+
+(function () {
+  const backToTop = document.querySelector(".back-to-top");
+  if (!backToTop) return;
+
+  const toggleButton = () => {
+    if (window.scrollY > 300) {
+      backToTop.classList.add("show");
+    } else {
+      backToTop.classList.remove("show");
+    }
+  };
+
+  window.addEventListener("scroll", toggleButton, { passive: true });
+  toggleButton();
+})();
