01e50e514a04ad2e498938a0d56a53be0b6e9ca6
minimeartstudio <Laura.piantedosi@gmail.com>
2026-02-11 12:04:18 -0500

    Update main.js

--- journal/main.js
+++ journal/main.js
@@ -1,13 +1,1 @@
-(function () {
-  const header = document.querySelector(".topnav");
-  if (!header) return;
-
-  const onScroll = () => {
-    if (window.scrollY > 18) header.classList.add("scrolled");
-    else header.classList.remove("scrolled");
-  };
-
-  window.addEventListener("scroll", onScroll, { passive: true });
-  onScroll();
-})();
 
